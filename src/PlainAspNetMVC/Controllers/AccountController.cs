e9cbf5116b41d6c6437d9529dcf3c4661b88ba1f
rizamarhaban <riza.marhaban@hotmail.com>
2016-06-11 12:11:20 +0800

    Add project files.

---
+++ src/PlainAspNetMVC/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PlainAspNetMVC.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace PlainAspNetMVC.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public AccountController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AccountController>();
+        }
+
+        //
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                // For God sake, NEVER DO THIS! This is just an example!
+                if (!String.IsNullOrWhiteSpace(model.Email) && model.Password == "12345678")
+                {
+                    var claims = new List<Claim> {
+                        new Claim("ID", "1234567890"),
+                        new Claim("name", "It's me!"),
+                        new Claim("username", "admin"),
+                        new Claim("role", "admin") };
+
+                    var id = new ClaimsIdentity(claims, "password");
+                    await HttpContext.Authentication.SignInAsync("Cookies", new ClaimsPrincipal(id));
+
+                    return new LocalRedirectResult(returnUrl ?? "/Home");
+                }
+
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult forbidden()
+        {
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+    }
+}
---
+++ src/PlainAspNetMVC/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PlainAspNetMVC
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            #region Config Builder
+            var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build(); 
+            #endregion
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            #region Other Configuration
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            #endregion
+
+            #region Cookies
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies",
+                LoginPath = new PathString("/account/login"),
+                AccessDeniedPath = new PathString("/account/forbidden"),
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true
+            }); 
+            #endregion
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
